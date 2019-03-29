78c882dc5b0cc6bbcfbdaa1c917bd3715b3ac728
Carla Sullivan <sull0237@algonquinlive.com>
2019-03-29 14:49:02 -0400

    ajax

--- js/main.js
+++ js/main.js
@@ -22,13 +22,50 @@ $(document).ready(function ($) {
   // attribute method
   $('img:first').attr('src', './img/image-5.jpg')
   // class methods
-  console.log$('img:first').hasClass('special')
+  console.log($('img:first').hasClass('special'))
   // $('img').addClass('special')
   $('img').toggleClass('special')
 
   // events
   $('img').click(function(){
-    console.log($(this).attr('src'))
-    $(this.toggleClass('special'))
+    console.log ($(this).attr('src'))
+    $(this).toggleClass('special')
   })
-})
+
+  // AJAX
+// click on nav-link
+// stop the default
+// get te href
+// check the #content
+// load function
+  $('#content').load('./about.html')
+  $('#contentNav .nav-link') .click(function (e) {
+    e.preventDefault()
+    var page = $(this).attr('href')
+    $('#contentNav .active').removeClass('active')
+    $(this).addClass('active')
+    $('#content') .fadeOut(500, function (){
+        $(this).load(page)
+    }).fadeIn(500)
+  })  // closing click event on the contentNav nav-link
+  $.ajax({
+    url: './data/posts.json',
+    type: 'GET',
+    dataType: 'json'
+
+  }).done(function (data) {
+    var numPosts = data.posts.length
+    for(var i = 0; i <numPosts; i++){
+      var post = '<div class="col-sm-6 p-5"> <h3>'
+      post += (i+1) + '.' + data.posts[i].title
+      post+=  '</h3><p>'
+      post+= data.posts[i].body
+      post+= '</p></div>'
+      $('#posts') .append(post)
+
+    }
+
+    console.log(data)
+
+  })
+})  // closing the document.ready method and the function
